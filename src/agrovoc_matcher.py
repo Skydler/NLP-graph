19ba68654ef75eb020e810bbefb8d855f29cb14a
Leonel <leonelmandarino@gmail.com>
2021-09-13 15:48:36 -0300

    Fix API mistake, now really using SPARQL endpoint

--- src/agrovoc_matcher.py
+++ src/agrovoc_matcher.py
@@ -1,9 +1,9 @@
 import logging
 
-from rdflib import Graph, SKOS
-import requests as rq
+from SPARQLWrapper import SPARQLWrapper
+from rdflib import OWL, SKOS
 
-from consts import AGROVOC_PREFIX, BASE_AGROVOC_URL, LOCAL_GRAPH_PREFIX as PREFIX
+from consts import LOCAL_GRAPH_PREFIX as PREFIX, RELATED_CONCEPTS_QUERY
 from utils import normalize_resource
 
 
@@ -15,47 +15,27 @@ def extend_with_agrovoc(g, concept):
     normalized_concept = normalize_resource(concept)
 
     # Prevent further requests if it's already processed
-    if (PREFIX[normalized_concept], PREFIX["is_agrovoc"], None) in g:
+    if (PREFIX[normalized_concept], OWL.sameAs, None) in g:
         return
 
-    concept_URI = search_agrovoc_concept(concept)
-    if not concept_URI:
-        return
+    new_graph = query_agrovoc(concept)
+    if new_graph:
+        logging.info(f'Concept "{concept}" found in agrovoc, extracting data...')
+        concept_URI = get_concept_URI(new_graph)
 
-    logging.info(f'Concept "{concept}" found in agrovoc, extracting data...')
-    graph = create_remote_graph(concept_URI)
-
-    concept_ID = concept_URI.removeprefix("http://aims.fao.org/aos/agrovoc/")
-    g.add(
-        (
-            PREFIX[normalized_concept],
-            PREFIX["is_agrovoc"],
-            AGROVOC_PREFIX[concept_ID],
-        )
-    )
-
-    agrovoc_data = get_related_triplets(graph, concept_ID)
-    for triplet in agrovoc_data:
-        g.add(triplet)
+        # Add local connection to the graph created
+        g.add((PREFIX[normalized_concept], OWL.sameAs, concept_URI))
+        g += new_graph
 
 
-def search_agrovoc_concept(concept):
-    response = rq.get(f"{BASE_AGROVOC_URL}rest/v1/search?query={concept}*&lang=en")
-    response.raise_for_status()
-
-    data = response.json()
-
-    if URIS := data.get("results"):
-        return URIS[0].get("uri")  # Return the first match
-    return None
+def query_agrovoc(concept):
+    sparql = SPARQLWrapper("https://agrovoc.fao.org/sparql")
+    query = RELATED_CONCEPTS_QUERY.format(concept=concept)
+    sparql.setQuery(query)
+    results = sparql.queryAndConvert()
+    return results
 
 
-def create_remote_graph(URI):
-    g = Graph().parse(URI)
-    return g
-
-
-def get_related_triplets(g, subject_id):
-    broader_concepts = g.triples((AGROVOC_PREFIX[subject_id], SKOS.narrower, None))
-    narrower_concepts = g.triples((None, SKOS.broader, AGROVOC_PREFIX[subject_id]))
-    return list(broader_concepts) + list(narrower_concepts)
+def get_concept_URI(g):
+    subject, _, _ = next(g.triples((None, SKOS.narrower, None)))
+    return subject
--- src/consts.py
+++ src/consts.py
@@ -2,5 +2,17 @@ from rdflib import Namespace
 
 LOCAL_GRAPH_PREFIX = Namespace("prefix://")
 AGROVOC_PREFIX = Namespace("http://aims.fao.org/aos/agrovoc/")
-WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/"
 BASE_AGROVOC_URL = "https://agrovoc.fao.org/browse/"
+
+RELATED_CONCEPTS_QUERY = """
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+CONSTRUCT {{
+  ?subject skos:narrower ?narrower.
+  ?subject skos:broader ?broader
+}}
+WHERE {{
+  ?subject skos:prefLabel "{concept}"@en.
+  ?subject skos:narrower ?narrower.
+  ?subject skos:broader ?broader
+}}
+"""
